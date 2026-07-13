09c5653d14ceb083c822280598d234e21a5fc90a
NotoriousN9 <rohitbasu1337@gmail.com>
2026-07-13 12:49:51 +0530

    Initialising variables and Menu

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int choice = 0;
+
+        System.out.println("""
+                ============================================
+                         Student Management System
+                ============================================
+                    1. Add NEW Student.
+                    2. Remove EXISTING Student.
+                    3. Change Student ATTRIBUTES.
+                    4. Search an EXISTING Student.
+                    5. View EXISTING Students in Structure.
+                    6. Exit
+                """);
+
+        while(choice > 0 && choice < 7) {
+            System.out.println("Choice: ");
+            choice = scanner.nextInt();
+        }
 
     }
 }
--- src/Student.java
+++ src/Student.java
@@ -1,2 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Student {
+    private final int studentId;
+    private final String studentName;
+    private final int studentAge;
+
+    private List<Student> students = new ArrayList<>();
+
+    Student(int studentId, String studentName, int studentAge) {
+        this.studentId = studentId;
+        this.studentAge = studentAge;
+        this.studentName = studentName;
+    }
+
+    
 }
